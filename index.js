3c35ae7b047743db09e39d6e016c2e7c449de16d
Divytech <155970643+Divytech@users.noreply.github.com>
2024-03-09 13:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ async function main() {
   bot.start(async (ctx) => {
     try {
       ctx.reply(
-        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
+        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nIt can take upto 10 seconds to bypass link so wait..\n\nMade with ❤️ by @devbots2\n\nSend any terabox link to download.`,
         Markup.inlineKeyboard([
-          Markup.button.url(" Channel", "https://t.me/botcodes123"),
-          Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
+          Markup.button.url(" Channel", "https://t.me/devbots2"),
+          Markup.button.url("Report bug", "https://t.me/champaklalbot"),
         ]),
       );
     } catch (e) {
